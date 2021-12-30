31e457c295979cdf0b9c076c2e9a9a13a53afbb8
Gloria Ding <gloria.hc.ding@gmail.com>
2021-12-29 19:50:10 -0800

    Create api basics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.create(title: 'Make Bed', isDone: false)
+Todo.create(title: 'Take out the trash', isDone: false)
